6a6c1cc5c737624a4a59d26b0e8522f9c0d604b2
RyanDBarnett <conner2468@gmail.com>
2019-05-16 09:24:13 -0600

    Fetch reservations from api and put them in App state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchReservations();
+  }
+
+  fetchReservations = () => {
+    const url = 'http://localhost:3001/api/v1/reservations'
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(reservations => {
+      this.setState({ reservations })
+    })
+  }
+
   render() {
     return (
       <div className="App">
