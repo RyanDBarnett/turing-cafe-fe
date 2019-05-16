814078d529939cbd897058062ad6219aef9e1dd5
RyanDBarnett <conner2468@gmail.com>
2019-05-16 10:13:50 -0600

    Make ReservationCard functional instead of component

--- src/ReservationCard.js
+++ src/ReservationCard.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ReservationCard.css'
 
-class ReservationCard extends Component {
-
-  render() {
-    const {date, id, name, number, time, cancelReservation} = this.props
-    return (
-      <article className="Card">
-        <h2>{name}</h2>
-        <p>{date}</p>
-        <p>{time}</p>
-        <p>Numer of guests: {number}</p>
-        <button onClick={() => cancelReservation(id)}>Cancel</button>
-      </article>
-    )
-  }
+const ReservationCard = ({date, id, name, number, time, cancelReservation}) => {
+  return (
+    <article className="Card">
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Numer of guests: {number}</p>
+      <button onClick={() => cancelReservation(id)}>Cancel</button>
+    </article>
+  )
 }
 
 export default ReservationCard;
