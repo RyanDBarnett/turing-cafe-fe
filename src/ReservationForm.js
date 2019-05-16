78af25afe4faf7142fd6d19ac84861d3ee284b58
RyanDBarnett <conner2468@gmail.com>
2019-05-16 09:41:30 -0600

    Create form html and default state

---
+++ src/ReservationForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './ReservationForm.css';
+
+class ReservationForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: 0
+    }
+  }
+
+  render() {
+    const {name, date, time, number} = this.state;
+    return (
+      <form onSubmit={this.handleSubmit} className="ReservationForm">
+        <input onChange={this.handleChange} type='text' name='name' placeholder='Name' value={name} />
+        <input onChange={this.handleChange} type='text' name='date' placeholder='Date (mm/dd)' value={date} />
+        <input onChange={this.handleChange} type='text' name='time' placeholder='Time' value={time} />
+        <input onChange={this.handleChange} type='text' name='number' placeholder='Number of guests' value={number}/>
+        <input type='submit' vale='Make Reservation'/>
+      </form>
+    )
+  }
+}
+
+export default ReservationForm;
